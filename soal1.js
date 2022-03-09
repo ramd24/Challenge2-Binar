7351946fdb8a9e0ea112b1e510f6be73c47c486f
Ramdayyy <ramday24@gmail.com>
2022-03-09 16:03:31 +0700

    soal 1

--- soal1.js
+++ soal1.js
@@ -1,1 +1,11 @@
-console.log('qqq');
+function changeWord ( selectedText, changedText, text ){
+    let result = text.replace(selectedText, changedText);
+
+    return result;
+}
+
+const kalimat1 = 'Andini sangat mencintai kamu selamanya';
+const kalimat2 = 'Gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu';
+
+console.log(changeWord('mencintai', 'membenci', kalimat1));
+console.log(changeWord('bromo', 'semeru', kalimat2));
