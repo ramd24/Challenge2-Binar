eff04ee9e5ac21c72a2fb6f5c95b6ae8f778d47b
Ramdayyy <ramday24@gmail.com>
2022-03-11 17:19:51 +0700

    soal 3

--- soal3.js
+++ soal3.js
@@ -1,5 +1,19 @@
 function checkEmail (email) {
-
+    let validEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g
+    let invalidEmail = /^([a-z\d\.-]+)@([a-z\d-]+)$/
+    
+    if ( typeof(email) == 'undefined' ) {
+      return 'ERROR: email cannot be empty';
+    }
+    if ( email.match(validEmail) ) {
+      return 'VALID';
+    }
+    if ( email.match(invalidEmail) ){
+      return 'INVALID';
+    }
+    if ( typeof(email) == 'string' && !email.match(validEmail) ) {
+      return 'ERROR: use a valid email address';
+    }
 }
 
 console.log(checkEmail('apranata@binar.co.id'));
